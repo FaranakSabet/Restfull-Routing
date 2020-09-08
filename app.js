2d4c21d7671d04e8dac6f93adfe96c4c50e286e6
faranak Sabet <faranaksabet@yahoo.com>
2020-09-08 14:57:24 -0700

    updated

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
-var bodyParser = require(bodyParser);
+var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var express = require("express");
 var app = express();
 
+//APP CONFIG
 mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+//MONGOOSE/MODEL CONFIG
+var blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: { type: Date, default: Date.now },
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+Blog.create({
+  title: "Test Blog",
+  image:
+    "https://www.thesprucepets.com/thmb/sfuyyLvyUx636_Oq3Fw5_mt-PIc=/3760x2820/smart/filters:no_upscale()/adorable-white-pomeranian-puppy-spitz-921029690-5c8be25d46e0fb000172effe.jpg",
+  body: "HELLO THIS IS A BLOG POST",
+});
+//RESTFUL ROUTES
 
 app.listen(3000, function () {
   console.log("SERVER IS RUNNING");
